2ef259c41a82089edfebe12243cb5e640f16ae7d
Nayak Satya <nayaksatya2012@gmail.com>
2026-09-03 00:17:00 +0300

    Added another practice lab

---
+++ MftPython/PythonPractice/calculator.py
@@ -0,0 +1,9 @@
+#this is just a practice program for basic calculator
+
+first = float(input("Enter the first number: "))
+second = float(input("Enter the second number: "))
+
+print("sum:", first + second)
+print("Difference:", first - second)
+print("product:", first * second)
+print("Division:", first / second)
