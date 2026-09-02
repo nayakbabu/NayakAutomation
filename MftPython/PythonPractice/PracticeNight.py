318f8f698c0a345f46ce913ae261b6c1707b1ec4
Nayak Satya <nayaksatya2012@gmail.com>
2026-09-02 23:56:11 +0300

    Added another student version

--- MftPython/PythonPractice/PracticeNight.py
+++ MftPython/PythonPractice/PracticeNight.py
@@ -1,3 +1,15 @@
 print("hello Nayak , good night")
 
 print("I am practicing Python with Nayak Sir ")
+
+
+name = "Rakesh"
+age = 12
+height = 1.65
+
+is_learning = True
+
+print(name)
+print(age)
+print(height)
+print(is_learning)
