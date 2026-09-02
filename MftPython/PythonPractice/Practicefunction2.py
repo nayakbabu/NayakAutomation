e0baac4cc29b25d57667ab5f8912577f04ca07d1
Nayak Satya <nayaksatya2012@gmail.com>
2026-09-02 18:42:33 +0300

    Added some more function v3

--- MftPython/PythonPractice/Practicefunction2.py
+++ MftPython/PythonPractice/Practicefunction2.py
@@ -10,8 +10,20 @@
 print(is_valid_port(8000))
 print(is_valid_port(70000))'''
 
-def is_valid_port(port):
+'''def is_valid_port(port):
     return 1 <= port <= 65535
 
 print(is_valid_port(8000))
-print(is_valid_port(70000))
+print(is_valid_port(70000))'''
+'''def pizza_order(toppings):
+    return "your Pizza has: " + ", ".join(toppings)
+print(pizza_order(["Cheese","Mashrrom", "Olives"]))'''
+
+'''def favorite_fruit(name):
+    return name + " is my favorite fruit !"
+print(favorite_fruit("Mango"))'''
+
+def superhero_name(your_name):
+    return your_name + " The unthinkable "
+print(superhero_name("Nayak, "))
+print(superhero_name("Rudvik, "))
