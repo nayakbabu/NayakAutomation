81c6578642aa0012544bbcb0e64aa5c8b681eb93
Nayak Satya <nayaksatya2012@gmail.com>
2026-09-03 23:46:12 +0300

    Added few student practice

--- MftPython/PythonPractice/ifstatement.py
+++ MftPython/PythonPractice/ifstatement.py
@@ -4,3 +4,20 @@ if age >= 18:
     print("You are an adult in India. ")
 else:
     print("You are an underage minor of India. ")
+
+score = int(input("Enter your score: "))
+
+if score >= 90:
+    print("You are an excellent student")
+elif score >= 60:
+    print("You are an above average student")
+elif score >= 40:
+    print("You are an average student")
+else:
+    print("You are a worst student")
+
+for number in range(1, 6):
+    print(number)
+
+for age in range(24, 30):
+    print(age)
