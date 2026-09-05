1c4ec4749fa1230d4b3bb6ed87205da33e34489a
Nayak Satya <nayaksatya2012@gmail.com>
2026-09-05 21:46:53 +0300

    Added few functions practice lab

---
+++ MftPython/PythonPractice/function.py
@@ -0,0 +1,15 @@
+# In this class we will learn function
+# def should be the starting means "define a function"
+#function should have a name
+
+def greet(name):
+    print("Hello, " + name + "!")
+greet("Satya")
+greet("Sam")
+
+#This is another function for your practice
+
+def introduce(name, city):
+    print("My name is " + name + ".")
+    print("I live in " + city + ".")
+introduce("Nayak", "Sofia")
