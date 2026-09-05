fc34178073c17d5f31e6632585091d151f582c55
Nayak Satya <nayaksatya2012@gmail.com>
2026-09-05 22:13:13 +0300

    Added few more list use cases

--- MftPython/PythonPractice/list.py
+++ MftPython/PythonPractice/list.py
@@ -1,4 +1,16 @@
 fruits = ["Apple", "Banana", "Orange", "Pinapple"]
+fruits.append("Mango")
 print(fruits)
 print(fruits[0])
 print(fruits[1])
+
+for fruit in fruits:
+    print("I like " + fruit)
+
+
+# This is another list example 
+
+
+sports = ["Crciket", "Hockey", "Football", "Tennis"]
+for sport in sports:
+    print("I like " + sport)
