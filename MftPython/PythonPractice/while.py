534e3ca99be0ef18aace23dd622bd0d5fd8a8e6d
Nayak Satya <nayaksatya2012@gmail.com>
2026-09-05 20:53:10 +0300

    Added another practice

--- MftPython/PythonPractice/while.py
+++ MftPython/PythonPractice/while.py
@@ -2,3 +2,15 @@ count = 1
 while count <= 5:
     print(count)
     count = count + 1
+
+
+
+foods = ["pizza", "sushi", "Biriyani"]
+
+for food in foods:
+    print("I like", food)
+
+fruits = ["Banana", "Apple", "Orange"]
+
+for fruit in fruits:
+    print("I like", fruit)
