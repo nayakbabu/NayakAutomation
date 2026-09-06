16630caf6a5de691950bc4f662401c0806eeb7cf
Nayak Satya <nayaksatya2012@gmail.com>
2026-09-06 11:52:25 +0300

    Added some list practice for students

--- MftPython/PythonPractice/list.py
+++ MftPython/PythonPractice/list.py
@@ -14,3 +14,11 @@ for fruit in fruits:
 sports = ["Crciket", "Hockey", "Football", "Tennis"]
 for sport in sports:
     print("I like " + sport)
+
+
+# This is another list practice for students 
+
+Capations = ["Kapil Dev", "Md Azhar", "Sachin Tendulkar", "Sourav Ganguly", "MS Dhoni", "Virat Kohili"]
+
+for caption in Capations:
+    print("The Indian crciket team caption was " + caption)
