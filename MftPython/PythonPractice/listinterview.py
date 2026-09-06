68f2c36c23aa460e3ea824d1f677ff3491e8ff95
Nayak Satya <nayaksatya2012@gmail.com>
2026-09-06 13:07:11 +0300

    added few more examples for lists

--- MftPython/PythonPractice/listinterview.py
+++ MftPython/PythonPractice/listinterview.py
@@ -6,3 +6,13 @@ odd = [x for x in list if x % 2 != 0]
 
 print("Even:", even)
 print("Odd:", odd)
+
+
+#another list practice
+
+list1 = [11,12,13,14,15,16,17,18,19,20]
+
+odd1 = [x for x in list1 if x % 2 != 0]
+even1 = [x for x in list1 if x % 2 == 0]
+print("Odd:", odd1)
+print("even:", even1)
