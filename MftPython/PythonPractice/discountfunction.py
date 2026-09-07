1caa396a8f195a7a74534df128df41537b076ed5
Nayak Satya <nayaksatya2012@gmail.com>
2026-09-07 20:34:18 +0300

    Added few more use cases

--- MftPython/PythonPractice/discountfunction.py
+++ MftPython/PythonPractice/discountfunction.py
@@ -7,3 +7,13 @@ def calculate_discount(price, discount_percent):
 
 new_price = calculate_discount(100, 20)
 print("Final price:", new_price )
+
+# This is another program practice with logic area of rectangle 
+
+def calculate_area(length, width):
+    rectangle_area = length * width
+    return rectangle_area
+
+
+area = calculate_area(5, 4)
+print(area)
