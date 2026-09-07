c5478a342e4f5dd8e4dc62a162a67da38dbd3468
Nayak Satya <nayaksatya2012@gmail.com>
2026-09-07 20:18:27 +0300

    Added few more functions for practice

---
+++ MftPython/PythonPractice/functionreturn.py
@@ -0,0 +1,14 @@
+#This code we will write for a function to return value 
+def add_numbers(first, second):
+    total = first + second
+    return total
+answer = add_numbers(5, 3)
+print(answer)
+
+# This is another example for your practice students 
+
+def mult_numbers(first, second):
+    total = first * second
+    return total
+correctanswer = mult_numbers(5, 6)
+print(correctanswer)
