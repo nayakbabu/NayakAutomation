22b6e89971ea7f8718e07649febc326a35263479
Nayak Satya <nayaksatya2012@gmail.com>
2026-09-14 14:35:16 +0300

    Added a new function use case

--- MftPython/PythonPractice/function.py
+++ MftPython/PythonPractice/function.py
@@ -63,3 +63,9 @@ def maximum(a, b):
     else:
         return b
 print(maximum(12 , 6))
+
+# we will write a function to reverse a word
+
+def reverse_word(word):
+    return word[::-1]
+print(reverse_word("GITHUB"))
